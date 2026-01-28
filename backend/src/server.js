a2cf4adfa9cbe40f61008650fc717e36c3485a09
Shivprasad Pravin Dongapure <prasaddongapure7660@gmail.com>
2026-01-28 15:35:24 +0530

    inital commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,8 +2,12 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import Path from "path";
 
 const app = express();
+
+
+const __dirname = Path.resolve();
 const port = process.env.PORT || 3000;
 
 console.log((process.env.PORT));
